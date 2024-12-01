7b3bdacfdcfd4a927f8ceea7a26c6b7eb0629280
Marc <marc@beatentrack.games>
2024-12-02 00:03:08 +0100

    First commit

---
+++ day1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = System.IO.File.ReadAllText(@"input.txt");
+            int sum = 0;
+            Console.WriteLine(input);
+        }
+    }
+}
